182b510289e10eb97828d14e66e80d64cba9a4fd
Ghanmidev2012 <242857573+Ghanmidev2012@users.noreply.github.com>
2026-01-19 11:39:35 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import time
+from termcolor import colored
+ImportError
+#loading 
+print (colored("ATLAS SERIA 1.0","blue"))
+time.sleep(2)
+print (colored("loading sys","green"))
+time.sleep(2)
+print (colored("loading files","green"))
+time.sleep (2)
+print (colored("loading your data","green"))
+time.sleep(2)
+print(colored("------ATLAS SERIA------","blue"))
+#app
+print ("1/setting")
+print("2/notebook")
+print("3/help")
+print("4/ai")
+#setting
+if input()=="1":
+    print("format sys")
+    print("accont")
+    print ("data")
+if input()=="format":
+    print(colored("are you sure for format you sys","red"))
+if input()=="yes":
+    print ("your sys is format")
+    print (colored("ATLAS SERIA 1.0","blue"))
+time.sleep(2)
+print (colored("loading sys","green"))
+time.sleep(2)
+print (colored("loading files","green"))
+time.sleep (2)
+print (colored("loading your data","green"))
+time.sleep(2)
+print(colored("------ATLAS SERIA------","blue"))
+
+if input()=="accont":
+    print(colored("you don't active save sys to save your accont","red"))
+
+
